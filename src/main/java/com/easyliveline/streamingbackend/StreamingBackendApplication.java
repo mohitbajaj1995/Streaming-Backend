81115b65aedd0fe6a64a30b425674a7004d3ed8b
mohitbajaj1995 <mohitbajaj1995@gmail.com>
2025-05-22 17:47:54 +0530

    Initialize

---
+++ src/main/java/com/easyliveline/streamingbackend/StreamingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.easyliveline.streamingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easyliveline/streamingbackend/StreamingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyliveline.streamingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
